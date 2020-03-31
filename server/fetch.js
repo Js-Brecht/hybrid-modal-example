c43ea9c7beb92e344c5203c24d088e0707afd39a
Js-Brecht <myrlin1@gmail.com>
2020-03-31 13:26:37 -1000

    follow unsplash api requirements

--- server/fetch.js
+++ server/fetch.js
@@ -11,7 +11,7 @@ const Promise = Bluebird;
 
 global.fetch = fetch;
 
-// const unsplash = new Unsplash.default({ accessKey: process.env.GATSBY_UNSPLASH_ACCESS_KEY });
+const unsplash = new Unsplash.default({ accessKey: process.env.GATSBY_UNSPLASH_ACCESS_KEY });
 const lorem = new LoremIpsum({
     sentencesPerParagraph: {
         max: 8,
@@ -47,31 +47,29 @@ exports.getData = async () => {
 
     if ((await fs.exists(dataCache))) {
         data = new Map(JSON.parse((await fs.readFile(dataCache)).toString()));
+    } else {
+        data = new Map();
+        const images = await unsplash.search.photos('people', 1, 10, {
+            orientation: 'portrait',
+        }).then(Unsplash.toJson);
+        for (const [idx, image] of images.results.entries()) {
+            const dataNode = {
+                id: idx + 1,
+                name: lorem.generateWords(2),
+                age: Math.floor(Math.random() * (maxAge - minAge)) + minAge,
+                image
+            }
+            const imagePath = getImagePath(image.id);
+            if (!(await fs.exists(imagePath))) {
+                unsplash.photos.downloadPhoto(image);
+                const imageData = await fetch(image.links.download).then((res) => res.buffer());
+                await fs.writeFile(imagePath, imageData);
+                image.links.download = getImageDownloadLink(image.id)
+            }
+            data.set(image.id, dataNode);
+        }
+        await fs.writeFile(dataCache, JSON.stringify(Array.from(data)));
     }
-
-    // else {
-    //     data = new Map();
-    //     const images = await unsplash.search.photos('people', 1, 10, {
-    //         orientation: 'portrait',
-    //     }).then(Unsplash.toJson);
-    //     for (const [idx, image] of images.results.entries()) {
-    //         const dataNode = {
-    //             id: idx + 1,
-    //             name: lorem.generateWords(2),
-    //             age: Math.floor(Math.random() * (maxAge - minAge)) + minAge,
-    //             image
-    //         }
-    //         const imagePath = getImagePath(image.id);
-    //         if (!(await fs.exists(imagePath))) {
-    //             unsplash.photos.downloadPhoto(image);
-    //             const imageData = await fetch(image.links.download).then((res) => res.buffer());
-    //             await fs.writeFile(imagePath, imageData);
-    //             image.links.download = getImageDownloadLink(image.id)
-    //         }
-    //         data.set(image.id, dataNode);
-    //     }
-    //     await fs.writeFile(dataCache, JSON.stringify(Array.from(data)));
-    // }
     
     return Array.from(data.values());
 }
@@ -82,7 +80,7 @@ exports.getImage = async (imageId, resizeOpts) => {
 
     if (!data) await this.getData();
 
-    // unsplash.photos.downloadPhoto(data.get(imageId).image)
+    unsplash.photos.downloadPhoto(data.get(imageId).image)
     return new Promise(async (resolve) => {
         if (resizeOpts.width !== undefined || resizeOpts.height !== undefined) {
             sharp(filePath)
